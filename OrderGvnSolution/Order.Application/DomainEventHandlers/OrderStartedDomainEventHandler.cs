3f6c61843e523a1deee0b30ce63efd820a2b7291
guven.uysal <guven.uysal@teknosol.com.tr>
2022-02-15 00:00:06 +0300

    Add domain event

---
+++ OrderGvnSolution/Order.Application/DomainEventHandlers/OrderStartedDomainEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Order.Application.Repository;
+using Order.Domain.Events;
+
+namespace Order.Application.DomainEventHandlers
+{
+    public class OrderStartedDomainEventHandler:INotificationHandler<OrderStartedDomainEvent>
+    {
+        private readonly IBuyerRepository _buyerRepository;
+
+        public OrderStartedDomainEventHandler(IBuyerRepository buyerRepository)
+        {
+            _buyerRepository = buyerRepository;
+        }
+
+        public async Task Handle(OrderStartedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
